d9d556ffd82f292ad6ab4441de03f76884daf81b
Jiayun <lijiayun22@mails.tsinghua.edu.cn>
2025-03-03 20:04:01 +0800

    Update build file

--- build.sh
+++ build.sh
@@ -26,12 +26,6 @@ make -j
 
 cd ../../../
 
-echo "Uncompress vocabulary ..."
-
-cd Vocabulary
-tar -xf ORBvoc.txt.tar.gz
-cd ../../..
-
 echo "Configuring and building ORB_SLAM3 ..."
 
 colcon build
